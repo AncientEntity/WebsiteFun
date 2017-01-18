4b6a78235d5cf98066cb270e6d523f224189421c
AncientEntity <rogoclub@outlook.com>
2017-01-17 21:51:36 -0800

    Added blog
    added blog

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function BottomInfo() {
 	Breaking(4);
 	document.write("<hr width='60%'/> ");
 	document.write("	<p>");
-	document.write("	This website was created using <strong>html</strong><br />		and thanks to <a href='http://github.com/'><strong>github</strong></a> we can host it freely.")
+	document.write("	This website was created using <strong>html</strong><br />		and thanks to <a href='http://github.com/'><strong>github</strong></a> we can host it freely. <br /> Making all this possible.")
 	document.write("	</p>");
 	document.write("<br /> <a href='#Top'>Back To Top.</a>")
 	Breaking(1);
@@ -34,9 +34,10 @@ function TopMenu() {
 	document.write("<hr />");
 	document.write("<table cellwidth='50' border='0' width='500' >");
 	document.write("	<tr>");
-	document.write("		<th><a href='index.html'><h2 title='The Home.'>Home</h2></a></td>");
-	document.write("		<th><a href='contact.html'><h2 title='The Contact.'>Contact</h2></a></td>");
-	document.write("		<th><a href='about.html'><h2 title='The About.'>About</h2></a></td>");
+	document.write("		<th><a href='index.html'><h2 title='The Home.'>Home</h2></a></th>");
+	document.write("		<th><a href='blog.html'><h2 title='The Blog.'>Blog</h2></a></th>");
+	document.write("		<th><a href='contact.html'><h2 title='The Contact.'>Contact</h2></a></th>");
+	document.write("		<th><a href='about.html'><h2 title='The About.'>About</h2></a></th>");
 	document.write("	</tr>");
 	document.write("</table>");
 	document.write("");
